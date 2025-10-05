37ec75748eb04bf98881d19bc4b3838c5efb3c52
hades <anselmedupuy@gmail.com>
2025-10-05 13:50:13 +0200

    labyrinth first take on DFS

--- labyrinth.py
+++ labyrinth.py
@@ -1,52 +1,128 @@
 # Labyrinthe représenté par une matrice (0 = chemin, 1 = mur)
 # Entrée en haut [0][1] et sortie en bas [9][8]
+#IA generated labyrinth representation with pathfinding visualization
 labyrinth = [
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-    [1, 1, 1, 0, 1, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-    [1, 1, 1, 0, 1, 1, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0]  # Exit at [9][9]
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],  # Exit at [29][28]
 ]
 
-start = (0, 1)
-paths = []
-end = (9, 9)
-walls = []
-connectedPaths = []
 
-connections = [(0,1),(1,0),(0,-1),(-1,0)]
+#IA generated function to display the labyrinth and the path found by DFS with colors
+def display_labyrinth(lab, path=None):
+    # ANSI color codes
+    RESET = '\033[0m'
+    WALL = '\033[90m'  # Dark gray for walls
+    PATH = '\033[91m'  # Green for found path
+    NORMAL = '\033[97m'  # White for regular paths
+    
+    print("\nLabyrinth:")
+    for i in range(len(lab)):
+        for j in range(len(lab[0])):
+            if path and (i,j) in path:
+                print(f"{PATH}▪{RESET}", end=" ")  # Path found by DFS
+            elif lab[i][j] == 1:
+                print(f"{WALL}█{RESET}", end=" ")  # Wall
+            else:
+                print(f"{NORMAL}▫{RESET}", end=" ")  # Regular path
+        print()  # New line after each row
+
+start = (0, 1)
+end = (29, 28)
+
+connections = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 def dfs(labyrinth, start, end):
-    queue = [start]
-    while queue:
-        current = queue[0]
-        visited = {end: None}
+    queue = [start] #add all paths to queue
+    visited = {start: None} #visited = {end: None} at each iteration
 
-        for x, y in paths:
-            for directionX, directionY in connections:
-                tempX, tempY = x+directionX, y+directionY
-                if (tempX, tempY) in paths:
-                    queue.append((tempX, tempY))
-                    visited[(tempX, tempY)] = (x, y)
-        print(queue)
-        print(visited)
-        queue.pop(0)
-        if current == end:
-            return print("path found")
+    paths = getAllPaths(labyrinth)
+    connectedPaths = removeDuplicateConnections(paths)
+
+    while queue: 
+        current = queue[0] #current = first element of queue
 
 
 
-
-    for i in range(0,10):
-        for j in range(0,10):
-            if labyrinth[i][j] == 0:
-                paths.append((i,j))
+        for path in connectedPaths: #for each connection in connected paths
+            if current in path: #if current is in the connection
+                nextPath = path[1] if path[0] == current else path[0] #nextPath = path[1] if path[0] == current else nextPath = path[0]
+                if nextPath not in queue and nextPath not in visited: #if nextPath is not in queue and not in visited
+                    queue.append(nextPath) #add nextPath to queue
+                    queue.pop(0) #remove first element of queue
+                    visited[nextPath] = current #add nextPath to visited with current as value
+                    # print("added", nextPath)
+        if current == end: #if current is end
+            return visited
                 
 
-finalPath = dfs(labyrinth, start, end)
-print(finalPath)
+        
+
+#all 0's
+def getAllPaths(labyrinth):
+    paths = []
+    for i in range(len(labyrinth)):
+        for j in range(len(labyrinth[0])):
+            if labyrinth[i][j] == 0:
+                paths.append((i,j))
+    return paths
+
+#all connections between 0's (= paths)
+def getAllConnections(paths):
+    connectedPaths = []
+    for x, y in paths:
+        for directionX, directionY in connections:
+            tempX, tempY = x+directionX, y+directionY
+            if (tempX, tempY) in paths:
+                connectedPaths.append(((x,y),(tempX,tempY)))
+    return connectedPaths
+
+#remove duplicates connections
+def removeDuplicateConnections(paths):
+    connectedPaths = getAllConnections(paths)
+    for path in connectedPaths:
+        if path in connectedPaths and (path[1], path[0]) in connectedPaths:
+            connectedPaths.remove((path[1], path[0]))
+    return connectedPaths
+
+
+path = dfs(labyrinth, start, end)
+
+final_path = []
+current = end
+while current is not None:  
+    final_path.append(current) 
+    current = path[current] 
+
+final_path.reverse()
+
+print("Shortest path:", final_path)
+display_labyrinth(labyrinth, final_path)
+    
